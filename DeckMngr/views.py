70cd458555e942addd600563cf7f418e13bb0827
kouheyHEY <vudtarm@gmail.com>
2023-08-01 00:52:42 +0900

    first commit

---
+++ DeckMngr/views.py
@@ -0,0 +1,6 @@
+from DeckMngr import app
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
---
+++ server.py
@@ -0,0 +1,4 @@
+from DeckMngr import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
